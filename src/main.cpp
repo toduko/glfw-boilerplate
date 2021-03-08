21efdd323ce32487fc49bd5acf600563edacce66
Todor Dukov <teddyepich@gmail.com>
2021-03-08 09:59:53 +0200

    Added glew

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
-#define GLFW_INCLUDE_NONE
 #include <stdio.h>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
-#include <GL/gl.h>
 
 int main()
 {
@@ -22,10 +21,50 @@ int main()
 
     glfwMakeContextCurrent(window);
 
+    GLenum err = glewInit();
+    if (err != GLEW_OK)
+    {
+        printf("glewInit() failed: %s\n", glewGetErrorString(err));
+    };
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
+
+    static const GLfloat g_vertex_buffer_data[] = {
+        -1.0f,
+        -1.0f,
+        0.0f,
+        1.0f,
+        -1.0f,
+        0.0f,
+        0.0f,
+        1.0f,
+        0.0f,
+    };
+
+    GLuint vertexbuffer;
+    glGenBuffers(1, &vertexbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-        glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+        glEnableVertexAttribArray(0);
+        glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+        glVertexAttribPointer(
+            0,
+            3,
+            GL_FLOAT,
+            GL_FALSE,
+            0,
+            (void *)0
+        );
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDisableVertexAttribArray(0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
